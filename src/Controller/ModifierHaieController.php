6ddf5ffb1f9a07724a1b4be9d0d141c02c9c2dfb
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-12-20 15:09:49 +0100

    Update Haie and Categorie entities, add SensioFrameworkExtraBundle

--- src/Controller/ModifierHaieController.php
+++ src/Controller/ModifierHaieController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Haie;
 use App\Form\HaieType;
 use App\Form\ModifType;
 use App\Repository\HaieRepository;
+use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,18 +34,22 @@ class ModifierHaieController extends AbstractController
     public function liste(HaieRepository $haieRepository): Response
     { 
         $haies = $haieRepository->findAll();
+
+        
         return $this->render('modifier_haie/index.html.twig', array('mesHaies' => $haies));
             
     }
 
     #[Route('/modifier/{code}', name: 'app_haie_modifierUneHaie')]
-    public function modif(string $code ,EntityManagerInterface $em ,Request $request): Response
+    public function modif(string $code ,EntityManagerInterface $em ,CategorieRepository $cr ,  Request $request): Response
     {
 
      
 
         $haie = $em->getRepository(Haie::class)->find($code);
-        return $this->render('LaHaiemodifier_haie/index.html.twig', ['haie' => $haie]);
+        $categories = $cr->findAll();
+
+        return $this->render('LaHaiemodifier_haie/index.html.twig', ['haie' => $haie , 'categories' => $categories]);
     }
 
     #[Route('/modifier/', name: 'app_haie_modifLaHaie')]
@@ -55,13 +61,20 @@ class ModifierHaieController extends AbstractController
         $prix=$request->get('prix');
         $code=$request->get('code');
         $codeModif=$request->get('codeModif');
+        $categorie=$request->get('categorie');
+        
 
 
         $haie = $em->getRepository(Haie::class)->find($codeModif);
         $haie->setNom($nom);
         $haie->setPrix($prix);
         $haie->setCode($code);
+
+        $Lacategorie = $em->getRepository(Categorie::class)->find($categorie);
+        $haie->setCategorie($Lacategorie);
+
         $em->persist($haie);
+
         $em->flush();
         return $this->redirectToRoute('app_modifier_list');
 
